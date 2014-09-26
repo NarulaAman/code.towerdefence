a48e707ab33dd4849362239323ea37320dad650b
amand.narula@gmail.com <amand.narula@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-09-26 00:01:42 +0000

    Added the map validator

---
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/MapValidator.java
@@ -0,0 +1,45 @@
+package ca.concordia.soen6441.logic;
+
+public class MapValidator implements IMapValidator {
+
+	@Override
+	public boolean isValid(Map map) {
+		
+		
+		if (mapHasNoEnd(map))
+		{
+			return false;
+		}
+		if (mapHasNoStart(map))
+		{
+			return false;
+		}
+		if (mapStartPositionCount(map) > 1)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
+	private int mapStartPositionCount(Map map) {
+		if (map.getStartTile() == null)
+		{
+			return 0;
+		}
+//		else if (map.getStartTile() != null)
+//		{
+			return 1;
+//		}
+	}
+
+	private boolean mapHasNoStart(Map map) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean mapHasNoEnd(Map map) {
+		return map.getEndTile() == null;
+	}
+
+}
