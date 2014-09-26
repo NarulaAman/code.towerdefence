f45b70e761991e184f5c1927a11558cb004da4fa
ankitchitlangia09@gmail.com <ankitchitlangia09@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-09-26 05:25:06 +0000

    Added DialogPanel and Modified MapFrame

---
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/DialogPanel.java
@@ -0,0 +1,41 @@
+package towerdefense.gui;
+
+import javax.swing.Box;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class DialogPanel extends JPanel{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+    private JTextField xField = null;
+    private JTextField yField = null;
+	public DialogPanel()
+	{
+		  xField = new JTextField(5);
+		  yField = new JTextField(5);
+	      
+		  add(new JLabel("x:"));
+	      add(xField);
+	      add(Box.createHorizontalStrut(15)); // a spacer
+	      add(new JLabel("y:"));
+	      add(yField);
+	}
+	
+	public int getXField()
+	{
+		return Integer.parseInt(xField.getText());
+		
+	}
+	
+	public int getYField()
+	{
+		return Integer.parseInt(yField.getText());
+		
+	}
+
+}
