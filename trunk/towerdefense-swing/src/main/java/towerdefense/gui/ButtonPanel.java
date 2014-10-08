82fcdcb2446c8578cf7973373456bfffde77a3ac
ankitchitlangia09@gmail.com <ankitchitlangia09@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-08 23:13:22 +0000

    User-driven allocation of grid elements such as scenery, path, entry point and exit point.

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/ButtonPanel.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/ButtonPanel.java
@@ -2,9 +2,15 @@ package towerdefense.gui;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ButtonPanel extends JPanel {
@@ -19,33 +25,79 @@ public class ButtonPanel extends JPanel {
 	private JButton path_button;
 	private JButton scenery_button;
 	private JButton save_button;
-	
-	
+	public static boolean path = false;
+	public static boolean scenery = false;
+
 	public ButtonPanel()
 	{
 		setVisible(true);
-		setBackground(Color.black);
-		setLayout(new GridLayout(6,1,4,4));
-		new_button = new JButton("New");
-		entry_button = new JButton("Entry");
-		exit_button = new JButton("Exit");
-		path_button = new JButton("Path");
-		scenery_button = new JButton("Scenery");
-		save_button = new JButton("Save");
+		setBackground(Color.gray);
+		
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		new_button = new JButton(new ImageIcon("newb.png"));
+		entry_button = new JButton(new ImageIcon("entry.png"));
+		exit_button = new JButton(new ImageIcon("exit.png"));
+		path_button = new JButton(new ImageIcon("path.png"));
+		scenery_button = new JButton(new ImageIcon("scenery.png"));
+		save_button = new JButton(new ImageIcon("save.png"));
 		
 		
-		entry_button.setBackground(new Color(215, 19, 19));
-		exit_button.addActionListener(new ExitButton());
+		new_button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				int dialogResult = JOptionPane.showConfirmDialog (null, "Would You Like to Save your Game?","Warning",JOptionPane.YES_NO_OPTION);
+				if(dialogResult == JOptionPane.YES_OPTION);
+				{
+					
+				}
+			}
+		});
+		
+		exit_button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+				
+			}
+		});
+		
+		path_button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				path = true;
+				scenery = false;
+			}
+		});
+		
+		scenery_button.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				scenery = true;	
+				path = false;
+			}
+		});
+
 		add(new_button);
 		add(entry_button);
 		add(exit_button);
 		add(path_button);
 		add(scenery_button);		
 		add(save_button);
-		
-		
+
+
+
+
 	}
-	
-	
+
+
 
 }
