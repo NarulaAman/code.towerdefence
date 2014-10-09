76ce3f628614d62289fc52419450e88ce82966fa
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-09 22:59:47 +0000

    Gave main method to MapEditionPanel for testing purposes

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/MapEditionPanel.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/MapEditionPanel.java
@@ -8,6 +8,7 @@ import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -146,5 +147,31 @@ public class MapEditionPanel extends JPanel {
 		
 	
 	}
+	
+	
+	private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        //Add the ubiquitous "Hello World" label.
+        Map map = new Map(4, 4);
+        MapEditionPanel mapEditionPanel = new MapEditionPanel(map);
+        frame.getContentPane().add(mapEditionPanel);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
 
 }
