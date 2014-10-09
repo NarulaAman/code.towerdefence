07b6ff587c48d7d41829c33d2a761b3eec86faff
mohamed.omar.t.a@gmail.com <mohamed.omar.t.a@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-09 22:05:16 +0000

    MapEditionPanel Created

---
+++ towerdefense-swing/src/main/java/towerdefense/gui/MapEditionPanel.java
@@ -0,0 +1,23 @@
+package towerdefense.gui;
+
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
+public class MapEditionPanel extends JPanel {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	GridPanel gridPanel;
+	
+
+	public MapEditionPanel() {
+		// TODO Auto-generated constructor stub
+		setLayout(new GridLayout(2,2,1,1));
+		
+	}
+
+}
