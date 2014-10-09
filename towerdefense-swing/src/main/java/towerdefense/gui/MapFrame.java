38d28df2e91b79b9f8472edeaed43a82aaf0c476
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-09 22:59:47 +0000

    Gave main method to MapEditionPanel for testing purposes

--- towerdefense-swing/src/main/java/towerdefense/gui/MapFrame.java
+++ towerdefense-swing/src/main/java/towerdefense/gui/MapFrame.java
@@ -55,7 +55,7 @@ public class MapFrame extends JFrame {
 
 
 		map = new Map(xVal, yVal);
-		gridPanel = new GridPanel(map);
+//		gridPanel = new GridPanel(map);
 		buttonPanel = new ButtonPanel();
 
 
