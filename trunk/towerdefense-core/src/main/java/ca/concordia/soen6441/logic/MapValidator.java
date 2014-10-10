69aa7b7b10f69e7bea878837cb12d73b0279b630
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-10 00:53:44 +0000

    Deleted the interface for map validation and added commets

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/MapValidator.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/MapValidator.java
@@ -1,5 +1,78 @@
 package ca.concordia.soen6441.logic;
 
-public interface MapValidator {
-	boolean isValid(Map map, StringBuilder messageIfNotValid);
+import ca.concordia.soen6441.logic.primitives.Coordinate;
+
+public class MapValidator {
+
+	public boolean isValid(Map map, StringBuilder messageIfNotValid) {
+
+		boolean mapInconsistent = false;
+
+		if (mapHasNoEnd(map)) {
+			messageIfNotValid.append("Map has no end\n");
+			mapInconsistent = true;
+		}
+		if (mapHasNoStart(map)) {
+			messageIfNotValid.append("Map has no end\n");
+			mapInconsistent = true;
+		}
+		if (mapStartPositionCount(map) > 1) {
+			messageIfNotValid.append("Map has more than one start position\n");
+			mapInconsistent = true;
+		}
+		if (mapStartIsNotInTheSides(map)) {
+			mapInconsistent = true;
+
+		}
+		if (mapInconsistent) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/**
+	 * Returns true if the start of the map is not on one of its sides
+	 * 
+	 * @param map
+	 *            map to be checked
+	 * @return true if the start of the map is not on one of its sides
+	 */
+	public boolean mapStartIsNotInTheSides(Map map) {
+		Coordinate startTile = map.getStartTile();
+
+		if (startTile.getX() == 0 || startTile.getY() == 0
+				|| startTile.getX() == map.getWidth() - 1
+				|| startTile.getY() == map.getHeight() - 1) {
+
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	
+	/**
+	 * @param map
+	 * @return
+	 */
+	public int mapStartPositionCount(Map map) {
+		if (map.getStartTile() == null) {
+			return 0;
+		}
+		// else if (map.getStartTile() != null)
+		// {
+		return 1;
+		// }
+	}
+
+	public boolean mapHasNoStart(Map map) {
+
+		return map.hasStartTile() == false;
+	}
+
+	public boolean mapHasNoEnd(Map map) {
+		return map.hasEndTile() == false;
+	}
+
 }
