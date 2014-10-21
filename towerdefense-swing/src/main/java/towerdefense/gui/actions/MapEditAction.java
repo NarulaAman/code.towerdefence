4a246a7b7ec84d5c6d1eda13635bffdd86728abc
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-21 23:32:00 +0000

    Created the MapEditAction, which will open the MapEditionDialog with the selected map in the StartGameDialog

---
+++ towerdefense-swing/src/main/java/towerdefense/gui/actions/MapEditAction.java
@@ -0,0 +1,39 @@
+package towerdefense.gui.actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import towerdefense.gui.MapEditionDialog;
+import towerdefense.gui.MapListPanel;
+import towerdefense.gui.MapListPanel.MapSelectionListener;
+import ca.concordia.soen6441.logic.Map;
+
+public class MapEditAction extends AbstractAction implements MapSelectionListener {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7403090617352119267L;
+	private final MapEditionDialog mapEditionDialog;
+	private Map selectedMap = null;
+	
+	public MapEditAction(MapEditionDialog mapEditionDialog) {
+		this.mapEditionDialog = mapEditionDialog;
+		setEnabled(false);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		mapEditionDialog.setMap(selectedMap);
+		mapEditionDialog.setVisible(true);
+	}
+
+	@Override
+	public void mapSelected(Map map) {
+		selectedMap = map;
+		setEnabled(selectedMap != null);
+		
+	}
+
+}
