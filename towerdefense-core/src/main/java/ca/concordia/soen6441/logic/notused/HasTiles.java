f75e048c7536811851eb6b7bd55ea0ef262edf93
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-21 23:52:08 +0000

    Remove uneeded imports from the core project

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/notused/HasTiles.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/notused/HasTiles.java
@@ -1,7 +1,6 @@
 package ca.concordia.soen6441.logic.notused;
 
 import ca.concordia.soen6441.logic.Tile;
-import ca.concordia.soen6441.logic.Tile.TileType;
 
 public interface HasTiles {
 	Tile tileOn(int x, int y);
