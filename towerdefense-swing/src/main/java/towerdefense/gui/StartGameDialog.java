0bbdec8478a9ee30976498dc452839b5d0652fd5
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 00:06:55 +0000

    Commented out the main method of StartGameDialog, should now use the RunMe class

--- towerdefense-swing/src/main/java/towerdefense/gui/StartGameDialog.java
+++ towerdefense-swing/src/main/java/towerdefense/gui/StartGameDialog.java
@@ -78,22 +78,21 @@ public class StartGameDialog extends JDialog implements MapListPanel.MapSelectio
 		
 	}
 
-	
-	private static void createAndShowGUI() {
-        StartGameDialog startPanel = new StartGameDialog(new MapJavaSerializationPersister(), null, null);
-        
-        startPanel.setVisible(true);
-    }
- 
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
+//	private static void createAndShowGUI() {
+//        StartGameDialog startPanel = new StartGameDialog(new MapJavaSerializationPersister(), null, null);
+//        
+//        startPanel.setVisible(true);
+//    }
+// 
+//    public static void main(String[] args) {
+//        //Schedule a job for the event-dispatching thread:
+//        //creating and showing this application's GUI.
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                createAndShowGUI();
+//            }
+//        });
+//    }
 
 
 	@Override
