d7cdc7069a6e76b66abbc2022dea4f393030aa88
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 18:26:10 +0000

    Added comments to GamePlay

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/GamePlay.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/GamePlay.java
@@ -7,11 +7,13 @@ import java.util.Observable;
 
 import ca.concordia.soen6441.logic.primitives.GridPosition;
 
+
+/**
+ * This class has logic for various operations executed on Game Play
+ *
+ */
 public class GamePlay extends Observable implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	final GameMap gameMap;
@@ -21,6 +23,10 @@ public class GamePlay extends Observable implements Serializable {
 	
 	int lives;
 
+	/**
+	 * @param gameMap The map selected for Play
+	 * @param currency The amount available
+	 */
 	public GamePlay(GameMap gameMap, int currency) {
 		super();
 		this.gameMap = gameMap;
@@ -29,6 +35,11 @@ public class GamePlay extends Observable implements Serializable {
 
 	}
 
+	/**
+	 * Validate if the user can buy and place the {@link Tower} or not
+	 * @param tower The object of class {@link Tower}
+	 * @return True if the user can buy tower else false
+	 */
 	public boolean buy(Tower tower) {
 		if (canPlace(tower) && tower.getBuyCost() <= currency) {
 			currency = currency - tower.getBuyCost();
@@ -40,10 +51,20 @@ public class GamePlay extends Observable implements Serializable {
 		}
 	}
 	
+	/**
+	 * Validate the position of {@link Tower}
+	 * @param tower The object of class {@link Tower}
+	 * @return True if the tower can be placed at give position else false
+	 */
 	public boolean canPlace(Tower tower) {
 		return getMap().canPlace(tower);
 	}
 
+	/**
+	 * Validate if the tower is present at give position
+	 * @param gridPosition The X and Y coordinate of {@link Tile} on {@link GameMap}
+	 * @return True if the Tower is present at give position
+	 */
 	public boolean hasTower(GridPosition gridPosition) {
 		for (Tower tower : towers) {
 			if (tower.getGridPosition().equals(gridPosition)) {
@@ -53,6 +74,11 @@ public class GamePlay extends Observable implements Serializable {
 		return false;
 	}
 
+	/**
+	 * Return the object of {@link Tower} at give position
+	 * @param gridPosition The X and Y coordinate of {@link Tile} on {@link GameMap}
+	 * @return The object of {@link Tower}
+	 */
 	public Tower getTower(GridPosition gridPosition) {
 		for (Tower tower : towers) {
 			if (tower.getGridPosition().equals(gridPosition)) {
@@ -66,8 +92,7 @@ public class GamePlay extends Observable implements Serializable {
 	 * Tries to upgrade the tower. Returns true if the upgrade happened, false
 	 * if not
 	 * 
-	 * @param tower
-	 *            to be upgraded
+	 * @param tower To be upgraded
 	 * @return true if the upgrade happened, false if not
 	 */
 	public boolean upgrade(Tower tower) {
@@ -88,6 +113,10 @@ public class GamePlay extends Observable implements Serializable {
 		notifyObservers();
 	}
 
+	/**
+	 * Remove the tower from {@link GameMap} and refund the currency amount
+	 * @param tower To be refunded
+	 */
 	public void sell(Tower tower) {
 		currency = currency + tower.getRefundRate();
 		towers.remove(tower);
@@ -95,24 +124,44 @@ public class GamePlay extends Observable implements Serializable {
 	}
 	
 
+	/**
+	 * Return the total number of towers 
+	 * @return The total number of towers on {@link GameMap}
+	 */
 	public int totalTowers() {
 		return towers.size();
 	}
 	
+	/**
+	 * Return amount of currency
+	 * @return currency
+	 */
 	public int getCurrency() {
 		return currency;
 	}
 
+	/**
+	 * Return the object of {@link GameMap}
+	 * @return {@link GameMap}
+	 */
 	public GameMap getMap() {
 		return gameMap;
 	}
 	
 	
 
+	/**
+	 * Return the list of Towers
+	 * @return List of Towers
+	 */
 	public List<Tower> getTowers() {
 		return new ArrayList<>(towers);
 	}
 
+	/**
+	 * Return the number of lives
+	 * @return Number of lives
+	 */
 	public int getLives() {
 		return lives;
 	}
