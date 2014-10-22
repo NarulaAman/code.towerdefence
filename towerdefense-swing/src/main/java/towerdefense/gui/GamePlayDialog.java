50f6b4e76ded6cd16e4a5feb742cdcc948e7cd93
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 18:04:14 +0000

    Commented the MapListModel

--- towerdefense-swing/src/main/java/towerdefense/gui/GamePlayDialog.java
+++ towerdefense-swing/src/main/java/towerdefense/gui/GamePlayDialog.java
@@ -179,48 +179,6 @@ public class GamePlayDialog extends JDialog implements TowerSelectedListener, Ma
 		
 	}
 
-	/**
-	 * Creates the Gui for testing purposes
-	 * @throws ClassNotFoundException
-	 * @throws IOException
-	 */
-	private static void createAndShowGUI() throws ClassNotFoundException, IOException {
-		//Create and set up the window.
-		JFrame frame = new JFrame("GamePlayPanel");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		GameMapDao gameMapDao = new GameMapJavaSerializationDao();
-		GameMap gameMap = gameMapDao.load("DefaultMap");
-		GamePlay gamePlay = new GamePlay(gameMap, 1000);
-		GamePlayDialog gamePlayPanel = new GamePlayDialog(gamePlay);
-		frame.setContentPane(gamePlayPanel);
-
-		//Display the window.
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	/**
-	 * Main method used for testing the GUI
-	 * @param args arguments are ignored by this method
-	 */
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					createAndShowGUI();
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-
 	/* (non-Javadoc)
 	 * @see towerdefense.gui.MapPanel.MapGridCoordinateClickedListener#mapGridCoordinateClicked(ca.concordia.soen6441.logic.primitives.GridPosition)
 	 */
@@ -266,5 +224,46 @@ public class GamePlayDialog extends JDialog implements TowerSelectedListener, Ma
 	public void update(Observable o, Object arg) {
 		readGamePlay();
 		
+	}
+	
+	/**
+	 * Creates the GUI for testing purposes
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	private static void createAndShowGUI() throws ClassNotFoundException, IOException {
+		//Create and set up the window.
+		JFrame frame = new JFrame("GamePlayPanel");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		GameMapDao gameMapDao = new GameMapJavaSerializationDao();
+		GameMap gameMap = gameMapDao.load("DefaultMap");
+		GamePlay gamePlay = new GamePlay(gameMap, 1000);
+		GamePlayDialog gamePlayPanel = new GamePlayDialog(gamePlay);
+		frame.setContentPane(gamePlayPanel);
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	/**
+	 * Main method used for testing the GUI
+	 * @param args arguments are ignored by this method
+	 */
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					createAndShowGUI();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 }
