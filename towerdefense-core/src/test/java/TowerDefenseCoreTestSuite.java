16da60afed761d1818510d9549fb5e474d9c66bb
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 18:52:18 +0000

    Added comments to TwerDefenseCoreTestSuite

--- towerdefense-core/src/test/java/TowerDefenseCoreTestSuite.java
+++ towerdefense-core/src/test/java/TowerDefenseCoreTestSuite.java
@@ -6,6 +6,10 @@ import ca.concordia.soen6441.logic.GamePlayTest;
 import ca.concordia.soen6441.logic.MapDaoTest;
 import ca.concordia.soen6441.logic.MapValidatorTest;
 
+/**
+ * This is the Test Suite of our TowerDefense core project
+ *
+ */
 @RunWith(Suite.class)
 @SuiteClasses({GamePlayTest.class, MapDaoTest.class, MapValidatorTest.class})
 public class TowerDefenseCoreTestSuite {
