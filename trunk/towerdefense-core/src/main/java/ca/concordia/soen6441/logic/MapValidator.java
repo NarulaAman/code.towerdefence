b4543b68daf2815514cb7a55a751a61386cff074
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 16:32:35 +0000

    Fixes to the MapValidatorTest, restructuring the internal test calls

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/MapValidator.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/MapValidator.java
@@ -8,6 +8,10 @@ import ca.concordia.soen6441.logic.primitives.GridPosition;
 
 public class MapValidator {
 	
+	private static final int START_POSITION_COUNT = 1;
+	private static final int END_POSITION_COUNT = 1;
+	private static final int MIN_DISTANCE_FROM_START_TO_EXIT = 2;
+
 	/**
 	 * check  validation of the gameMap 
 	 * 
@@ -23,29 +27,34 @@ public class MapValidator {
 
 		boolean mapInconsistent = false;
 
-		if (mapHasNoEnd(gameMap)) {
-			messageIfNotValid.append("GameMap has no end\n");
+		if (mapMustHaveEnd(gameMap)) {
+			messageIfNotValid.append("GameMap must have one end\n");
 			mapInconsistent = true;
 		}
-		if (mapHasNoStart(gameMap)) {
-			messageIfNotValid.append("GameMap has no start\n");
+		if (mapMustHaveStart(gameMap)) {
+			messageIfNotValid.append("GameMap must have one start\n");
 			mapInconsistent = true;
 		}
-		if (mapStartPositionCount(gameMap) > 1) {
-			messageIfNotValid.append("GameMap has more than one start position\n");
+		if (mapStartPositionCount(gameMap) > START_POSITION_COUNT) {
+			messageIfNotValid.append("GameMap shouldn't have more than one start position\n");
 			mapInconsistent = true;
 		}
-		if (coordinateIsNotInTheSides(gameMap, gameMap.getStartGridPosition())) {
+		
+		if (mapEndPositionCount(gameMap) > END_POSITION_COUNT) {
+			messageIfNotValid.append("GameMap shouldn't have more than one end position\n");
+			mapInconsistent = true;
+		}
+		if (! coordinateOnTheEdge(gameMap, gameMap.getStartGridPosition())) {
 			messageIfNotValid.append("GameMap start position must be on the edges\n");
 			mapInconsistent = true;
 		}
-		if (coordinateIsNotInTheSides(gameMap, gameMap.getEndGridPosition())) {
+		if (! coordinateOnTheEdge(gameMap, gameMap.getEndGridPosition())) {
 			messageIfNotValid.append("GameMap exit position must be on the edges\n");
 			mapInconsistent = true;
 		}
 			
-		if (distanceBtwnCoordinates(gameMap.getStartGridPosition(), gameMap.getEndGridPosition())) {
-			messageIfNotValid.append("GameMap must have distance between start and exit\n");
+		if (distanceBtwnCoordinates(gameMap.getStartGridPosition(), gameMap.getEndGridPosition()) > MIN_DISTANCE_FROM_START_TO_EXIT) {
+			messageIfNotValid.append("GameMap must have a distance between start and exit\n");
 			mapInconsistent = true;
 		}
 
@@ -56,15 +65,8 @@ public class MapValidator {
 		}
 	}
 	
-	public boolean distanceBtwnCoordinates(GridPosition positionA,GridPosition positionB) {
-		
-		double gridPositionDistance = Math.sqrt((positionA.getX()-positionB.getX())*(positionA.getX()-positionB.getX()) + (positionA.getY()-positionB.getY())*(positionA.getY()-positionB.getY()));
-		
-		if (gridPositionDistance<2) {
-			return false;
-		} else {
-			return true;
-		}
+	public double distanceBtwnCoordinates(GridPosition positionA,GridPosition positionB) {
+		return  Math.sqrt((positionA.getX()-positionB.getX())*(positionA.getX()-positionB.getX()) + (positionA.getY()-positionB.getY())*(positionA.getY()-positionB.getY()));
 	}
 
 	/**
@@ -74,7 +76,7 @@ public class MapValidator {
 	 *            gameMap to be checked
 	 * @return true if the start of the gameMap is not on one of its sides
 	 */
-	public boolean coordinateIsNotInTheSides(GameMap gameMap, GridPosition coordinate) {
+	public boolean coordinateOnTheEdge(GameMap gameMap, GridPosition coordinate) {
 		if (coordinate.getX() == 0 || coordinate.getY() == 0
 				|| coordinate.getX() == gameMap.getWidth() - 1
 				|| coordinate.getY() == gameMap.getHeight() - 1) {
@@ -86,41 +88,27 @@ public class MapValidator {
 	}
 
 	
-	/**
-	 * Returns 0 if the gameMap has only one start
-	 * @param gameMap  
-	 *            gameMap to be checked
-	 * @return 0 if the gameMap has only one start
-	 */
 	public int mapStartPositionCount(GameMap gameMap) {
 		if (gameMap.getStartGridPosition() == null) {
 			return 0;
 		}
-		// else if (gameMap.getStartTile() != null)
-		// {
 		return 1;
-		// }
+	}
+	
+	public int mapEndPositionCount(GameMap gameMap) {
+		if (gameMap.getEndGridPosition() == null) {
+			return 0;
+		}
+		return 1;
+	}
+	
+
+	public boolean mapMustHaveStart(GameMap gameMap) {
+		return gameMap.hasStartTile();
 	}
 
-	/**
-	 * Returns false if gameMap has no start
-	 * @param gameMap  
-	 *            gameMap to be checked
-	 * @return false if gameMap has no start
-	 */
-	public boolean mapHasNoStart(GameMap gameMap) {
-
-		return gameMap.hasStartTile() == false;
-	}
-
-	/**
-	 *  Returns false if gameMap has no end
-	 * @param gameMap
-	 *            gameMap to be checked
-	 * @return false if gameMap has no end
-	 */
-	public boolean mapHasNoEnd(GameMap gameMap) {
-		return gameMap.hasEndTile() == false;
+	public boolean mapMustHaveEnd(GameMap gameMap) {
+		return gameMap.hasEndTile();
 	}
 
 }
