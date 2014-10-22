7a050e265e2d8e80304d4fad00327aebf4c9c8a8
ankitchitlangia09@gmail.com <ankitchitlangia09@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 19:17:35 +0000

    Added comments to TowerLevelCharacteristics

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/TowerLevelCaracteristic.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/TowerLevelCaracteristic.java
@@ -1,5 +1,9 @@
 package ca.concordia.soen6441.logic;
 
+/**
+ * This class contain the characteristics of Tower as per level
+ *
+ */
 public class TowerLevelCaracteristic {
 
 	final int damage;
@@ -13,6 +17,13 @@ public class TowerLevelCaracteristic {
 	
 	final int range;
 
+	/**
+	 * Construct the tower characteristics
+	 * @param damage caused by the tower
+	 * @param buyCost of the tower
+	 * @param refundRate of the tower
+	 * @param range of the tower
+	 */
 	public TowerLevelCaracteristic(int damage, int buyCost, int refundRate, int range) {
 		super();
 		this.damage = damage;
@@ -21,22 +32,41 @@ public class TowerLevelCaracteristic {
 		this.range = range;
 	}
 
+	/**
+	 * Returns the damage caused by this tower
+	 * @return the damage caused by this tower
+	 */
 	public int getDamage() {
 		return damage;
 	}
 
+	/**
+	 * Returns the buy cost of this tower
+	 * @return the buy cost of this tower
+	 */
 	public int getBuyCost() {
 		return buyCost;
 	}
 
+	/**
+	 * Returns the refundRate cost of this tower
+	 * @return the refundRate cost of this tower
+	 */
 	public int getRefundRate() {
 		return refundRate;
 	}
 
+	/**
+	 * Returns the range cost of this tower
+	 * @return the range cost of this tower
+	 */
 	public int getRange() {
 		return range;
 	}
 
+	/** Returns the damage, buyCost, refundRate and range of tower
+	 * @return the damage, buyCost, refundRate and range of tower
+	 */
 	public String toHtmlString() {
 		return "<html>Damage: " + damage + "<br>Buy cost: "
 				+ buyCost + "<br>Refund: " + refundRate + "<br>Range: " + range
