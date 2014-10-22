5a2ab9f64ada0db156e786f6e8fa3d515cf93b82
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 16:32:35 +0000

    Fixes to the MapValidatorTest, restructuring the internal test calls

--- towerdefense-core/src/test/java/ca/concordia/soen6441/logic/MapValidatorTest.java
+++ towerdefense-core/src/test/java/ca/concordia/soen6441/logic/MapValidatorTest.java
@@ -1,12 +1,8 @@
 package ca.concordia.soen6441.logic;
 
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-
-import java.io.File;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,29 +11,29 @@ import ca.concordia.soen6441.logic.primitives.GridPosition;
 
 public class MapValidatorTest {
 	
-	private static final String MAP_DATA_FILENAME = "testMapName";
-	
-	static GameMap gameMap = new GameMap(32, 32);
+//	static GameMap gameMap = new GameMap(32, 32);
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		new File(MAP_DATA_FILENAME).delete();		
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(0, 0));
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(2, 4));
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(4, 27));
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(5, 8));
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(7, 14));
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(12, 26));
-		gameMap.set(Tile.ENEMY_PATH, new GridPosition(32, 32));
-		
-	}
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		new File(MAP_DATA_FILENAME).delete();
-	}	
+	private MapValidator mapValidator;
+	
+	private GameMap gameMap;
+//	
+//	@BeforeClass
+//	public static void setUpBeforeClass() throws Exception {
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(0, 0));
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(2, 4));
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(4, 27));
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(5, 8));
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(7, 14));
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(12, 26));
+//		gameMap.set(Tile.ENEMY_PATH, new GridPosition(32, 32));
+//		
+//	}
+
 
 	@Before
 	public void setUp() throws Exception {
+		mapValidator = new MapValidator();
+		gameMap = new GameMap(32, 32);
 	}
 
 	@After
@@ -57,22 +53,40 @@ public class MapValidatorTest {
 
 	@Test
 	public void testCoordinateIsNotInTheSides() {
-		fail("Not yet implemented");
+		
+		assertFalse(mapValidator.coordinateOnTheEdge(gameMap, new GridPosition(0, 0)));
+		assertFalse(mapValidator.coordinateOnTheEdge(gameMap, new GridPosition(1, 1)));
 	}
 
 	@Test
 	public void testMapStartPositionCount() {
-		fail("Not yet implemented");
+		assertEquals(0, mapValidator.mapStartPositionCount(gameMap));
+		gameMap.setStartGridPosition(new GridPosition(0, 0));		
+		assertEquals(1, mapValidator.mapStartPositionCount(gameMap));
+		gameMap.setStartGridPosition(new GridPosition(0, 1));
+		assertEquals(1, mapValidator.mapStartPositionCount(gameMap));
 	}
 
 	@Test
 	public void testMapHasNoStart() {
-		fail("Not yet implemented");
+		assertTrue(mapValidator.mapMustHaveStart(gameMap));
+	}
+	
+	@Test
+	public void testMapHasNoStartWithAStart() {
+		gameMap.setStartGridPosition(new GridPosition(0, 0));
+		assertFalse(mapValidator.mapMustHaveStart(gameMap));
 	}
 
 	@Test
 	public void testMapHasNoEnd() {
-		fail("Not yet implemented");
+		assertTrue(mapValidator.mapMustHaveEnd(gameMap));
+	}
+	
+	@Test
+	public void testMapHasNoEndWithAnEnd() {
+		gameMap.setEndGridPosition(new GridPosition(0, 0));
+		assertFalse(mapValidator.mapMustHaveEnd(gameMap));
 	}
 
 }
