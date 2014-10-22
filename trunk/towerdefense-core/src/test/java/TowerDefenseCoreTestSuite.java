3863d5a078ace3fea9e29205ed9d41aa7a21b055
ankitchitlangia09@gmail.com <ankitchitlangia09@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-22 15:49:09 +0000

    Added MapValidatorTest.class to TestSuite

--- trunk/towerdefense-core/src/test/java/TowerDefenseCoreTestSuite.java
+++ trunk/towerdefense-core/src/test/java/TowerDefenseCoreTestSuite.java
@@ -4,9 +4,10 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import ca.concordia.soen6441.logic.GamePlayTest;
 import ca.concordia.soen6441.logic.MapDaoTest;
+import ca.concordia.soen6441.logic.MapValidatorTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({GamePlayTest.class, MapDaoTest.class})
+@SuiteClasses({GamePlayTest.class, MapDaoTest.class, MapValidatorTest.class})
 public class TowerDefenseCoreTestSuite {
 	
 }
