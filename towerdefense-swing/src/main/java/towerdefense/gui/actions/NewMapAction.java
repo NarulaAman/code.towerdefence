5afcc59dc39c390804640b753185711832ecd86c
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-25 12:38:58 +0000

    Commented the NewMapAction

--- towerdefense-swing/src/main/java/towerdefense/gui/actions/NewMapAction.java
+++ towerdefense-swing/src/main/java/towerdefense/gui/actions/NewMapAction.java
@@ -8,48 +8,43 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import towerdefense.gui.MapEditionDialog;
-import towerdefense.gui.MapListPanel.MapSelectionListener;
 import towerdefense.gui.NewMapDialog;
 import ca.concordia.soen6441.logic.GameMap;
 
 
-public class NewMapAction extends AbstractAction implements MapSelectionListener {
-
+/**
+ * Action to create a new {@link GameMap} in the {@link MapEditionDialog}
+ *
+ */
+public class NewMapAction extends AbstractAction {
 	/**
 	 * 
 	 */
-	private static final Icon NEW_ICON = new ImageIcon(Object.class.getResource("/icons/newb.png"));
-    
-	
 	private static final long serialVersionUID = 7403090617352119267L;
+	private static final Icon NEW_ICON = new ImageIcon(Object.class.getResource("/icons/newb.png"));
+	private static final String MAP_SIZE_TEXT = "Enter the x and y values";
+	
 	private final MapEditionDialog mapEditionDialog;
-	private GameMap selectedMap = null;
 
-	private static final String newMapText = "Enter the x and y values";
-
+	/**
+	 * Constructs a {@link NewMapAction} to edit a map o a given {@link MapEditionDialog}
+	 * @param mapEditionDialog {@link MapEditionDialog} to 
+	 */
 	public NewMapAction(MapEditionDialog mapEditionDialog) {
 		super(null, NEW_ICON);
 		this.mapEditionDialog = mapEditionDialog;
 		setEnabled(true);
 	}
 
+	/**
+	 * Performs the {@link NewMapAction}
+	 * @param e not used - ignored
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
 		NewMapDialog newMapDialog = new NewMapDialog();
-		JOptionPane.showConfirmDialog(null, newMapDialog,newMapText,JOptionPane.OK_CANCEL_OPTION);
+		JOptionPane.showConfirmDialog(null, newMapDialog, MAP_SIZE_TEXT, JOptionPane.OK_CANCEL_OPTION);
 		mapEditionDialog.setMap(new GameMap(newMapDialog.getXField(), newMapDialog.getYField()));
 		mapEditionDialog.setVisible(true);
-
-		
-
-
     }
-	
-	@Override
-	public void mapSelected(GameMap gameMap) {
-		selectedMap = gameMap;
-		setEnabled(selectedMap != null);
-	}
-
 }
