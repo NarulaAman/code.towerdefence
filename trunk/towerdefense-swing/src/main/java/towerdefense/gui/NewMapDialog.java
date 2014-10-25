a52acf01af340329ed8ad6752fb540035a33c15e
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-25 14:07:44 +0000

    Code formatted the NewMapDialog

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/NewMapDialog.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/NewMapDialog.java
@@ -4,33 +4,30 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class NewMapDialog extends JPanel{
-        
-    /**
+public class NewMapDialog extends JPanel {
+
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 620779799725640674L;
-	
+
 	private JTextField xField = new JTextField(5);
-    private JTextField yField = new JTextField(5);
-    
-    public NewMapDialog()
-    {
-          add(new JLabel("x:"));
-          add(xField);
-         // add(Box.createHorizontalStrut(15)); // a spacer
-          add(new JLabel("y:"));
-          add(yField);
-    }
-    
-    public int getXField()
-    {
-            return Integer.parseInt(xField.getText());
-    }
-    
-    public int getYField()
-    {
-            return Integer.parseInt(yField.getText());
-    }
+	private JTextField yField = new JTextField(5);
+
+	public NewMapDialog() {
+		add(new JLabel("x:"));
+		add(xField);
+		// add(Box.createHorizontalStrut(15)); // a spacer
+		add(new JLabel("y:"));
+		add(yField);
+	}
+
+	public int getXField() {
+		return Integer.parseInt(xField.getText());
+	}
+
+	public int getYField() {
+		return Integer.parseInt(yField.getText());
+	}
 
 }
