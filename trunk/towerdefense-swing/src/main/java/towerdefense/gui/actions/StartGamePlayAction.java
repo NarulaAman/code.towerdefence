f15a7141db1196ddfad540c1b5d299dbccaeab5c
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-25 12:46:11 +0000

    Commented the StartGamePlayAction

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/actions/StartGamePlayAction.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/actions/StartGamePlayAction.java
@@ -7,10 +7,15 @@ import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 import towerdefense.gui.GamePlayDialog;
+import towerdefense.gui.MapEditionDialog;
 import towerdefense.gui.MapListPanel.MapSelectionListener;
 import ca.concordia.soen6441.logic.GameMap;
 import ca.concordia.soen6441.logic.GamePlay;
 
+/**
+ * Action to start playing the game on the {@link GamePlayDialog}
+ *
+ */
 public class StartGamePlayAction extends AbstractAction implements MapSelectionListener {
 	
 	/**
@@ -20,18 +25,29 @@ public class StartGamePlayAction extends AbstractAction implements MapSelectionL
 	private static final Icon START_ICON = new ImageIcon(Object.class.getResource("/icons/start.png"));
 	private GameMap selectedMap = null;
 	
+	/**
+	 * Constructs a {@link StartGamePlayAction}, new instances of the {@link GamePlayDialog} will be created
+	 * from inside this action for every new game
+	 */
 	public StartGamePlayAction() {
 		super(null, START_ICON);
-		
 		setEnabled(false);
 	}
 	
+	/**
+	 * Performs the {@link StartGamePlayAction}
+	 * @param e not used - ignored
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		GamePlayDialog gamePlayDialog = new GamePlayDialog(new GamePlay(selectedMap, 1000));
 		gamePlayDialog.setVisible(true);
 	}
 
+	/**
+	 * Invoked when a {@link GameMap} is selected to be played, can be null
+	 * @param gameMap {@link GameMap} to be played 
+	 */
 	@Override
 	public void mapSelected(GameMap gameMap) {
 		selectedMap = gameMap;
