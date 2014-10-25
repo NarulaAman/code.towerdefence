868438d0b81fa23af1a4c3b045f5a9f09d29895a
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-25 14:04:45 +0000

    Code formatted the NewMapDialog

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/NewMapDialog.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/NewMapDialog.java
@@ -11,14 +11,11 @@ public class NewMapDialog extends JPanel{
 	 */
 	private static final long serialVersionUID = 620779799725640674L;
 	
-	private JTextField xField = null;
-    private JTextField yField = null;
+	private JTextField xField = new JTextField(5);
+    private JTextField yField = new JTextField(5);
     
     public NewMapDialog()
     {
-          xField = new JTextField(5);
-          yField = new JTextField(5);
-          
           add(new JLabel("x:"));
           add(xField);
          // add(Box.createHorizontalStrut(15)); // a spacer
@@ -29,13 +26,11 @@ public class NewMapDialog extends JPanel{
     public int getXField()
     {
             return Integer.parseInt(xField.getText());
-            
     }
     
     public int getYField()
     {
             return Integer.parseInt(yField.getText());
-            
     }
 
 }
