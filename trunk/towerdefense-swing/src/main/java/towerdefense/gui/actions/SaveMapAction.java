0cab417697e5457d6c96c95615464f16481ef6cd
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-25 12:43:11 +0000

    Commented the SaveMapAction

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/actions/SaveMapAction.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/actions/SaveMapAction.java
@@ -9,8 +9,13 @@ import javax.swing.JOptionPane;
 import towerdefense.gui.MapEditionDialog;
 import towerdefense.gui.StartGameDialog;
 import ca.concordia.soen6441.io.GameMapDao;
+import ca.concordia.soen6441.logic.GameMap;
 import ca.concordia.soen6441.logic.MapValidator;
 
+/**
+ * Action to save a {@link GameMap} in the {@link MapEditionDialog}
+ *
+ */
 public class SaveMapAction implements ActionListener{
 	
 	private MapValidator mapValidator = new MapValidator();
@@ -18,37 +23,39 @@ public class SaveMapAction implements ActionListener{
 	private GameMapDao gameMapDao;
 	private StartGameDialog startGameDialog;
 
+	/**
+	 * Constructs a {@link SaveMapAction} to save the {@link GameMap} on the {@link MapEditionDialog} and to refresh
+	 * the given {@link StartGameDialog} when the save is successful
+	 * @param mapEditionDialog {@link MapEditionDialog} to have the {@link GameMap} saved
+	 * @param startGameDialog {@link StartGameDialog} to be refreshed when the save is done
+	 */
 	public SaveMapAction(MapEditionDialog mapEditionDialog, StartGameDialog startGameDialog) {
 		this.mapEditionDialog = mapEditionDialog;
 		this.startGameDialog = startGameDialog;
 		this.gameMapDao = mapEditionDialog.getMapDao() ;
 	}
 	
+
+	/**
+	 * Performs the {@link SaveMapAction}
+	 * @param e not used - ignored
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-            try {
-			
-			StringBuilder incorrectMap = new StringBuilder("");
+        try {
+			StringBuilder incorrectMap = new StringBuilder();
 			Boolean mapValidate = mapValidator.isValid(mapEditionDialog.getMap(), incorrectMap);
-
 
 			if(mapValidate) {
 				gameMapDao.save(mapEditionDialog.getMap(), mapEditionDialog.getMapName().getText());
 			    mapEditionDialog.setVisible(false);
 			    startGameDialog.refreshMaps();
-			    
 			} else {
-				JOptionPane.showMessageDialog(null, incorrectMap);
+				JOptionPane.showMessageDialog(mapEditionDialog, incorrectMap, "Map validation error",
+					    JOptionPane.ERROR_MESSAGE);
 			}
-		
-			
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
-		
-	}
-
-	public void setStartGameDialog(StartGameDialog startGameDialog) {
-		this.startGameDialog = startGameDialog;
 	}
 }
