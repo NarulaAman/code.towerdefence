8138bcfd1708acff92abdd16d101338b7b0f2141
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-26 17:33:09 +0000

    Game exits when closing the start window with the OS close button

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/StartGameDialog.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/StartGameDialog.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
@@ -58,6 +60,12 @@ public class StartGameDialog extends JDialog implements MapListPanel.MapSelectio
 		mapListPanel.addMapSelectionListerner(this);
 		mapListPanel.addMapSelectionListerner(mapEditAction);
 		mapListPanel.addMapSelectionListerner(startGamePlayAction);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		add(gridPanel, BorderLayout.CENTER);
 		setupSideBar(gameMapDao);
 		pack();
