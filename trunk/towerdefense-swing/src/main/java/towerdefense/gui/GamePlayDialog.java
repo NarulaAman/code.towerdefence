d56efd515e6aa4474fa09e770051919f8bbaac71
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-27 01:49:55 +0000

    Commented the main method that existed for testing purposes and moved it to the end of each main Dialog class: GamePlayDialog, MapEditionDialog, StartGameDialog

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/GamePlayDialog.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/GamePlayDialog.java
@@ -255,44 +255,44 @@ public class GamePlayDialog extends JDialog implements TowerSelectedListener, Ma
 		super.dispose();
 	}
 	
-	/**
-	 * Creates the GUI for testing purposes
-	 * @throws ClassNotFoundException
-	 * @throws IOException
-	 */
-	private static void createAndShowGUI() throws ClassNotFoundException, IOException {
-		//Create and set up the window.
-		JFrame frame = new JFrame("GamePlayPanel");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		GameMapDao gameMapDao = new GameMapJavaSerializationDao();
-		GameMap gameMap = gameMapDao.load("DefaultMap");
-		GamePlay gamePlay = new GamePlay(gameMap, 1000);
-		GamePlayDialog gamePlayPanel = new GamePlayDialog(gamePlay);
-		frame.setContentPane(gamePlayPanel);
-
-		//Display the window.
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	/**
-	 * Main method used for testing the GUI
-	 * @param args arguments are ignored by this method
-	 */
-	public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					createAndShowGUI();
-				} catch (ClassNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	/**
+//	 * Creates the GUI for testing purposes
+//	 * @throws ClassNotFoundException
+//	 * @throws IOException
+//	 */
+//	private static void createAndShowGUI() throws ClassNotFoundException, IOException {
+//		//Create and set up the window.
+//		JFrame frame = new JFrame("GamePlayPanel");
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//		GameMapDao gameMapDao = new GameMapJavaSerializationDao();
+//		GameMap gameMap = gameMapDao.load("DefaultMap");
+//		GamePlay gamePlay = new GamePlay(gameMap, 1000);
+//		GamePlayDialog gamePlayPanel = new GamePlayDialog(gamePlay);
+//		frame.setContentPane(gamePlayPanel);
+//
+//		//Display the window.
+//		frame.pack();
+//		frame.setVisible(true);
+//	}
+//
+//	/**
+//	 * Main method used for testing the GUI
+//	 * @param args arguments are ignored by this method
+//	 */
+//	public static void main(String[] args) {
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					createAndShowGUI();
+//				} catch (ClassNotFoundException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 }
--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/StartGameDialog.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/StartGameDialog.java
@@ -96,23 +96,6 @@ public class StartGameDialog extends JDialog implements MapListPanel.MapSelectio
 		
 	}
 
-//	private static void createAndShowGUI() {
-//        StartGameDialog startPanel = new StartGameDialog(new MapJavaSerializationPersister(), null, null);
-//        
-//        startPanel.setVisible(true);
-//    }
-// 
-//    public static void main(String[] args) {
-//        //Schedule a job for the event-dispatching thread:
-//        //creating and showing this application's GUI.
-//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//            public void run() {
-//                createAndShowGUI();
-//            }
-//        });
-//    }
-
-
 	/**
 	 * This method called when a certain map is being selected
 	 * @param gameMap that being selected
@@ -136,6 +119,21 @@ public class StartGameDialog extends JDialog implements MapListPanel.MapSelectio
 	 */
 	public void refreshMaps() {
 		mapListPanel.refreshList();
-		
 	}
+	
+//	private static void createAndShowGUI() {
+//  StartGameDialog startPanel = new StartGameDialog(new MapJavaSerializationPersister(), null, null);
+//  
+//  startPanel.setVisible(true);
+//}
+//
+//public static void main(String[] args) {
+//  //Schedule a job for the event-dispatching thread:
+//  //creating and showing this application's GUI.
+//  javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//      public void run() {
+//          createAndShowGUI();
+//      }
+//  });
+//}
 }
