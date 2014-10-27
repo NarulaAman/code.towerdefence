5bc6750e0e4439e9085f25dc1c1e6bea80dbcdbf
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-27 19:41:32 +0000

    Tag for the Build1 of the project

---
+++ tags/Build1/towerdefense-core/src/main/java/ca/concordia/soen6441/io/GameMapJavaSerializationDao.java
@@ -0,0 +1,84 @@
+package ca.concordia.soen6441.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.concordia.soen6441.logic.GameMap;
+import ca.concordia.soen6441.logic.GameMapDao;
+
+/**
+ * This class saves and loads the {@link GameMap} to a file given the
+ * map name.
+ *
+ */
+public class GameMapJavaSerializationDao implements GameMapDao {
+	
+	/**
+	 * This is the extension used in the filename
+	 */
+	public static final String MAP_FILENAME_EXTENSION = ".map";
+	
+	/**
+	 * This is the name of the file
+	 */
+	public static final String FILENAME_STRING_FORMAT = "." + File.separator + "%s" + MAP_FILENAME_EXTENSION;
+	
+
+	/**
+	 * Save to file a {@link GameMap} given a map name
+	 * @param gameMap {@link GameMap} to be saved
+	 * @param mapName name of the Map
+	 */
+	@Override
+	public void save(GameMap gameMap, String mapName) throws IOException { 
+		File file = new File(String.format(FILENAME_STRING_FORMAT, mapName)); 
+		gameMap.setName(mapName);
+		FileOutputStream fileOutputStream = new FileOutputStream(file); 
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+		objectOutputStream.writeObject(gameMap);
+		objectOutputStream.close();
+		fileOutputStream.close();
+		
+	}
+	
+	/**
+	 * Load the {@link GameMap} from the file.
+	 * @param mapName {@link GameMap} to be loaded
+	 */
+	@Override
+	public GameMap load(String mapName) throws IOException, ClassNotFoundException {
+		File file = new File(String.format(FILENAME_STRING_FORMAT, mapName));
+		FileInputStream fileInputStream = new FileInputStream(file);
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+		GameMap gameMap = (GameMap) objectInputStream.readObject();
+		objectInputStream.close();
+		fileInputStream.close();
+		return gameMap;
+	}
+
+	/**
+	 * List the name of all the saved {@link GameMap}.
+	 * @return the list of all map names
+	 */
+	@Override
+	public List<String> listAllNames() throws IOException {
+		File directory = new File(".");
+		List<String> fileList = new ArrayList<>();
+ 		for (File file : directory.listFiles())
+		{
+			if (file.isFile() && file.getName().endsWith(MAP_FILENAME_EXTENSION)) {
+				String filename = file.getName();
+				String mapName = filename.substring(0, filename.indexOf(MAP_FILENAME_EXTENSION));
+				fileList.add(mapName);
+			}
+		}
+		return fileList;
+	}
+	
+}
---
+++ tags/Build1/towerdefense-core/src/test/java/ca/concordia/soen6441/logic/GameMapDaoTest.java
@@ -0,0 +1,84 @@
+package ca.concordia.soen6441.logic;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import ca.concordia.soen6441.io.GameMapJavaSerializationDao;
+import ca.concordia.soen6441.logic.primitives.GridPosition;
+
+/**
+ * Test cases for the {@link GameMapDao}
+ *
+ */
+@FixMethodOrder(MethodSorters.JVM)
+public class GameMapDaoTest {
+	
+	private static final String MAP_DATA_FILENAME = "testMapName";
+	
+	GameMapJavaSerializationDao gameMapDao = new GameMapJavaSerializationDao();
+	static GameMap gameMap = new GameMap(32, 32);
+
+	/**
+	 * Creates a {@link GameMap} to aid the tests
+	 * @throws Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		gameMap.setTile(new GridPosition(1, 2), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(5, 6), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(2, 27), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(5, 8), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(3, 13), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(15, 26), Tile.ENEMY_PATH);
+		
+	}
+
+	/**
+	 * Delete the file created during the tests
+	 * @throws Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		new File(MAP_DATA_FILENAME).delete();
+	}	
+	
+	/**
+	 * Tests if an error occurs when opening a non-existant file
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	@Test(expected=IOException.class)
+	public final void testLoadWithNoExistantFile() throws ClassNotFoundException, IOException {
+		GameMap map2 = gameMapDao.load(MAP_DATA_FILENAME + "non-existant");
+		Assert.assertTrue(map2.equals(gameMap));
+	}
+
+	/**
+	 * Tests if the save happens without errors
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	@Test
+	public final void testSave() throws IOException, ClassNotFoundException {
+		gameMapDao.save(gameMap, MAP_DATA_FILENAME);
+	}
+
+	/**
+	 * Tests if the load method loaded the same method it saved before
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	@Test
+	public final void testLoad() throws ClassNotFoundException, IOException {
+		GameMap map2 = gameMapDao.load(MAP_DATA_FILENAME);
+		Assert.assertTrue(map2.equals(gameMap));
+	}
+
+}
