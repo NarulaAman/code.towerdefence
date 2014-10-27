c5777310d36633fbcf9b3ce2a91ad8f0918288c7
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-10-27 01:33:12 +0000

    Minor corrections before the tag for Build 1

--- trunk/towerdefense-core/src/test/java/ca/concordia/soen6441/logic/GamePlayTest.java
+++ trunk/towerdefense-core/src/test/java/ca/concordia/soen6441/logic/GamePlayTest.java
@@ -1,9 +1,9 @@
 package ca.concordia.soen6441.logic;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
