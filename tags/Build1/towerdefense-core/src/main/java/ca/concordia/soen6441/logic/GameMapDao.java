3e16c286b3308f3a7487fc8c8ec5871d10a82766
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-01 01:19:53 +0000

    Build1 Tag for delivery

---
+++ tags/Build1/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/GameMapDao.java
@@ -0,0 +1,38 @@
+package ca.concordia.soen6441.logic;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Interface for a Data Access Object for a {@link GameMap}
+ *
+ */
+public interface GameMapDao {
+
+	/**
+	 * Saves a {@link GameMap} by its name
+	 * @param gameMap to be saved
+	 * @param name to save the gameMap under
+	 * @throws IOException
+	 */
+	public void save(GameMap gameMap, String name) throws IOException;
+
+	/**
+	 * Load the {@link GameMap} by a name
+	 * @param name of the gameMap to be loaded
+	 * @return the gameMap loaded from file
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
+	public GameMap load(String name) throws IOException,
+			ClassNotFoundException;
+	
+	
+	/**
+	 * List the name of all the saved {@link GameMap}
+	 * @return The list of saved maps
+	 * @throws IOException
+	 */
+	public List<String> listAllNames() throws IOException;
+
+}
---
+++ tags/Build1/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/TowerFactory.java
@@ -0,0 +1,57 @@
+package ca.concordia.soen6441.logic;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ca.concordia.soen6441.logic.primitives.GridPosition;
+
+/**
+ * Responsible for creating the towers and holding their level characteristics
+ *
+ */
+public class TowerFactory {
+	
+	private Map<Class<? extends Tower>, List<TowerLevelCharacteristic>> towerTypeInformation = new HashMap<>();
+	
+	/**
+	 * Create a TowerFactory 
+	 */
+	public TowerFactory() {
+		List<TowerLevelCharacteristic> towerLevelCharacteristic  = new ArrayList<>();
+		towerLevelCharacteristic.add(new TowerLevelCharacteristic(10, 100, 75, 2));
+		towerLevelCharacteristic.add(new TowerLevelCharacteristic(15, 70, 75, 3));
+		towerTypeInformation.put(Tower.class, towerLevelCharacteristic);
+	}
+	
+	/**
+	 * Returns the maximum level of tower 
+	 * @param type The type of the tower
+	 * @return The maximum level of the tower
+	 */
+	public int maxLevel(Class<? extends Tower> type) {
+		return towerTypeInformation.get(type).size();
+	}
+	
+	/**
+	 * Return the object of {@link TowerLevelCharacteristic} based on tower type and level
+	 * @param type The type of the tower
+	 * @param level The level of the tower
+	 * @return The {@link TowerLevelCharacteristic}
+	 */
+	public TowerLevelCharacteristic getLevelInformation(Class<? extends Tower> type, int level) {
+		return towerTypeInformation.get(type).get(level-1);
+	}
+	
+	/**
+	 * Return the tower present on the given coordinate
+	 * @param type The type of the tower
+	 * @param coordinate The position of the tower on {@link GameMap}
+	 * @return The object of {@link Tower}
+	 */
+	public Tower towerOnCoordinate(Class<? extends Tower> type, GridPosition coordinate) {
+		return new Tower(1, coordinate, this); // new Tower(gridPosition, new TowerInformation(0, 0, 0, 0));
+	}
+	
+}
