403424a487c49de35509a8c8ce3277181f9be282
mohamed.omar.t.a@gmail.com <mohamed.omar.t.a@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-06 23:43:54 +0000

    Created data members and default constructor

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/Enemy.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/Enemy.java
@@ -1,5 +1,31 @@
 package ca.concordia.soen6441.logic;
 
+import ca.concordia.soen6441.logic.primitives.GridPosition;
+
 public class Enemy {
+	private GridPosition gridPosition;
+	private int health;
+	
+	public Enemy(){
+		gridPosition =null;
+		health=-1;
+	}
+	
+	
+	public GridPosition getGridPosition() {
+		return gridPosition;
+	}
+	public void setGridPosition(GridPosition gridPosition) {
+		this.gridPosition = gridPosition;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	
+	
+	
 
 }
