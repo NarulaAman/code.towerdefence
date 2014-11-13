ab29b75ceeff8e65ddaed7fdcd2bda35c57f6415
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-13 23:51:58 +0000

    Added comments to ShootClosestToEndPointStrategy file

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/shootingstrategy/ShootClosestToEndPointStrategy.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/shootingstrategy/ShootClosestToEndPointStrategy.java
@@ -3,10 +3,20 @@ package ca.concordia.soen6441.logic.tower.shootingstrategy;
 import java.util.List;
 
 import ca.concordia.soen6441.logic.Enemy;
+import ca.concordia.soen6441.logic.GamePlay;
 
+/**
+ * 
+ * This class has strategy to shoot the enemy closest to Exit 
+ *
+ */
 public class ShootClosestToEndPointStrategy extends AbstractShootingStrategy implements ShootingStrategy {
 
-	@Override
+/**
+ * Shoot the enemy closest to Exit point
+ * @param enemies The list of {@link Enemy} in the {@link GamePlay}
+ * @param enemyToShoot The {@link Enemy} to shoot 
+ */
 	public void shootIfInRange(List<Enemy> enemies) {
 		Enemy enemyToShoot = null;
 		for (Enemy enemy : enemies) {
