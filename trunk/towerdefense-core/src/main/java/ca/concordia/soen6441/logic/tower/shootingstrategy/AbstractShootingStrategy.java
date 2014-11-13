9622b736ffc880cf360efafc312c0dcf2371aa38
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-13 23:45:59 +0000

    Added comments to AbstractShootingStrategy file

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/shootingstrategy/AbstractShootingStrategy.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/shootingstrategy/AbstractShootingStrategy.java
@@ -1,20 +1,34 @@
 package ca.concordia.soen6441.logic.tower.shootingstrategy;
 
 import ca.concordia.soen6441.logic.tower.Tower;
+/**
+ * 
+ * This is abstract class for shooting strategies 
+ * of the tower
+ */
 
 public abstract class AbstractShootingStrategy implements ShootingStrategy {
 
 	private Tower tower;
-
+/**
+ *Call the constructor method of extended class.  
+ */
 	public AbstractShootingStrategy() {
 		super();
 	}
 
-	@Override
+/**
+* Set the value to Tower
+* @param tower {@link Tower} 
+*/
 	public void setTower(Tower tower) {
 		this.tower = tower;
 	}
-	
+
+/**
+ * Return the value of {@link Tower} 	
+ * @return {@link Tower} That will have strategy applied to
+ */
 	protected Tower getTower() {
 		return tower;
 	}
