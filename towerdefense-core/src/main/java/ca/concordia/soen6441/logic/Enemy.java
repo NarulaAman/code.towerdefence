99ee66e2ea0f1403ec8d0f647dd01e9e9523f082
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-14 00:08:10 +0000

    Commented out dead code

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/Enemy.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/Enemy.java
@@ -1,36 +1,34 @@
 package ca.concordia.soen6441.logic;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
 
 import javax.vecmath.Point2f;
 import javax.vecmath.Vector2f;
 
 import ca.concordia.soen6441.logic.primitives.GridPosition;
-import ca.concordia.soen6441.logic.tower.Tower;
 
 public class Enemy extends Observable {
-	private GridPosition titorGridPosition;
-	private int health;
-	private char[][] titorMap;
-	private boolean lock;
-	private GridPosition startGridPosition;
-	private GridPosition endGridPosition;
-	private List<Tower> towerList;
-	private List<Tower> towersInMap;
+//	private GridPosition titorGridPosition;
+
+//	private char[][] titorMap;
+//	private boolean lock;
+//	private GridPosition startGridPosition;
+//	private GridPosition endGridPosition;
+//	private List<Tower> towerList;
+//	private List<Tower> towersInMap;
 
 	private/* final */GameMap gameMap;
 	private Point2f currentPosition;
 	private float speed = 0.5f;
 	private int destinationIdx = 1;
 	private int prize = 50;
+	private int health;
 
-	public Enemy() {
-		 gameMap= null;
-		health = 0;
-		currentPosition = null;
-	}
+//	public Enemy() {
+//		 gameMap= null;
+//		health = 0;
+//		currentPosition = null;
+//	}
 
 	public Enemy(GameMap gameMap, int health, Point2f currentPosition) {
 		this.gameMap = gameMap;
@@ -38,51 +36,51 @@ public class Enemy extends Observable {
 		this.currentPosition = currentPosition;
 	}
 
-	public char[][] getMap() {
-		return titorMap;
-	}
+//	public char[][] getMap() {
+//		return titorMap;
+//	}
 
-	public Enemy(GridPosition p, int health, char[][] map, List<Tower> towersInMap) {
-		titorGridPosition = p;
-		this.health = health;
-		for (int i = 0; i < map.length; i++)
-			for (int j = 0; j < map[0].length; j++) {
-				if (map[i][j] == 'S') {
-					startGridPosition = new GridPosition(i, j);
-					char[][] copy = copyMap(map);
-					titorMap = copy;
-					solveMap(copy, i, j);
+//	public Enemy(GridPosition p, int health, char[][] map, List<Tower> towersInMap) {
+//		titorGridPosition = p;
+//		this.health = health;
+//		for (int i = 0; i < map.length; i++)
+//			for (int j = 0; j < map[0].length; j++) {
+//				if (map[i][j] == 'S') {
+//					startGridPosition = new GridPosition(i, j);
+//					char[][] copy = copyMap(map);
+//					titorMap = copy;
+//					solveMap(copy, i, j);
+//
+//				}
+//				if (map[i][j] == '@') {
+//					endGridPosition = new GridPosition(i, j);
+//				}
+//
+//			}
+//		towerList = new ArrayList<Tower>();
+//		this.towersInMap = towersInMap;
+//		lock = false;
+//	}
 
-				}
-				if (map[i][j] == '@') {
-					endGridPosition = new GridPosition(i, j);
-				}
+//	public GridPosition getGridPosition() {
+//		return titorGridPosition;
+//	}
+//
+//	public void setPosisiton(int x, int y) {
+//		titorGridPosition = new GridPosition(x, y);
+//	}
 
-			}
-		towerList = new ArrayList<Tower>();
-		this.towersInMap = towersInMap;
-		lock = false;
-	}
-
-	public GridPosition getGridPosition() {
-		return titorGridPosition;
-	}
-
-	public void setPosisiton(int x, int y) {
-		titorGridPosition = new GridPosition(x, y);
-	}
-
-	public char[][] copyMap(char[][] test1) {
-		char[][] test2 = new char[test1.length][test1[0].length];
-		for (int i = 0; i < test1.length; i++) {
-
-			for (int j = 0; j < test1[0].length; j++) {
-				test2[i][j] = test1[i][j];
-
-			}
-		}
-		return test2;
-	}
+//	public char[][] copyMap(char[][] test1) {
+//		char[][] test2 = new char[test1.length][test1[0].length];
+//		for (int i = 0; i < test1.length; i++) {
+//
+//			for (int j = 0; j < test1[0].length; j++) {
+//				test2[i][j] = test1[i][j];
+//
+//			}
+//		}
+//		return test2;
+//	}
 
 	public int getHealth() {
 		return health;
@@ -96,127 +94,127 @@ public class Enemy extends Observable {
 		}
 	}
 
-	public void lockTitor(Tower t) {
+//	public void lockTitor(Tower t) {
+//
+//		towerList.add(t);
+//		lock = true;
+//	}
 
-		towerList.add(t);
-		lock = true;
-	}
+//	public void unlockTitor(Tower t) {
+//		for (Tower i : towerList) {
+//			if (i.getGridPosition().getX() == t.getGridPosition().getX()
+//					&& i.getGridPosition().getY() == t.getGridPosition().getY()) {
+//				towerList.remove(i);
+//			}
+//		}
+//		if (towerList.isEmpty()) {
+//			lock = false;
+//		}
+//
+//	}
 
-	public void unlockTitor(Tower t) {
-		for (Tower i : towerList) {
-			if (i.getGridPosition().getX() == t.getGridPosition().getX()
-					&& i.getGridPosition().getY() == t.getGridPosition().getY()) {
-				towerList.remove(i);
-			}
-		}
-		if (towerList.isEmpty()) {
-			lock = false;
-		}
+//	public boolean titorStillAlive() {
+//		if (health > 0) {
+//			return true;
+//		} else
+//			return false;
+//	}
 
-	}
+//	public boolean solveMap(char[][] initialMap, int x, int y) {
+//
+//		if (x < 0 || x > initialMap.length - 1 || y < 0 || y > initialMap[0].length - 1) {
+//			return false;
+//		} else if (initialMap[x][y] == '@') {
+//			return true;
+//		} else if (initialMap[x][y] == '#' || initialMap[x][y] == 'X' || initialMap[x][y] == '!') {
+//			return false;
+//		} else if (initialMap[x][y] == '-' || initialMap[x][y] == 'S') {
+//			initialMap[x][y] = '/';
+//			titorMap[x][y] = '/';
+//			if (solveMap(initialMap, x + 1, y))
+//				return true;
+//			if (solveMap(initialMap, x - 1, y))
+//				return true;
+//			if (solveMap(initialMap, x, y + 1))
+//				return true;
+//			if (solveMap(initialMap, x, y - 1))
+//				return true;
+//			initialMap[x][y] = '!';
+//			titorMap[x][y] = '!';
+//			return false;
+//		}
+//		return false;
+//	}
 
-	public boolean titorStillAlive() {
-		if (health > 0) {
-			return true;
-		} else
-			return false;
-	}
-
-	public boolean solveMap(char[][] initialMap, int x, int y) {
-
-		if (x < 0 || x > initialMap.length - 1 || y < 0 || y > initialMap[0].length - 1) {
-			return false;
-		} else if (initialMap[x][y] == '@') {
-			return true;
-		} else if (initialMap[x][y] == '#' || initialMap[x][y] == 'X' || initialMap[x][y] == '!') {
-			return false;
-		} else if (initialMap[x][y] == '-' || initialMap[x][y] == 'S') {
-			initialMap[x][y] = '/';
-			titorMap[x][y] = '/';
-			if (solveMap(initialMap, x + 1, y))
-				return true;
-			if (solveMap(initialMap, x - 1, y))
-				return true;
-			if (solveMap(initialMap, x, y + 1))
-				return true;
-			if (solveMap(initialMap, x, y - 1))
-				return true;
-			initialMap[x][y] = '!';
-			titorMap[x][y] = '!';
-			return false;
-		}
-		return false;
-	}
-
-	public void newGridPositionVerify() {
-		towerList.clear();
-		lock = false;
-
-		for (Tower i : towersInMap) {
-
-			if (i.canLockTarget(this)) {
-
-				towerList.add(i);
-				lock = true;
-			}
-		}
-		if (lock) {
-			for (Tower i : towerList) {
-				i.shootEnemy(this);
-
-			}
-
-		}
-
-	}
-
-	public boolean move() {
-		if (titorGridPosition.getX() > -1 && titorGridPosition.getY() > -1) {
-
-			if (health > 0) {
-				if (titorGridPosition.getX() + 1 < titorMap.length
-						&& titorMap[titorGridPosition.getX() + 1][titorGridPosition.getY()] == '/') {
-
-					titorMap[titorGridPosition.getX() + 1][titorGridPosition.getY()] = '+';
-					titorGridPosition = new GridPosition(titorGridPosition.getX() + 1, titorGridPosition.getY());
-
-				} else if (titorGridPosition.getX() > 0
-						&& titorMap[titorGridPosition.getX() - 1][titorGridPosition.getY()] == '/') {
-
-					titorMap[titorGridPosition.getX() - 1][titorGridPosition.getY()] = '+';
-					titorGridPosition = new GridPosition(titorGridPosition.getX() - 1, titorGridPosition.getY());
-
-				} else if (titorGridPosition.getY() + 1 < titorMap[0].length
-						&& titorMap[titorGridPosition.getX()][titorGridPosition.getY() + 1] == '/') {
-
-					titorMap[titorGridPosition.getX()][titorGridPosition.getY() + 1] = '+';
-					titorGridPosition = new GridPosition(titorGridPosition.getX(), titorGridPosition.getY() + 1);
-
-				} else if (titorGridPosition.getY() > 0
-						&& titorMap[titorGridPosition.getX()][titorGridPosition.getY() - 1] == '/') {
-
-					titorMap[titorGridPosition.getX()][titorGridPosition.getY() - 1] = '+';
-					titorGridPosition = new GridPosition(titorGridPosition.getX(), titorGridPosition.getY() - 1);
-
-				}
-				newGridPositionVerify();
-				if (titorGridPosition.getX() == endGridPosition.getX()
-						&& titorGridPosition.getY() == endGridPosition.getY()) {
-					return false;
-				} else {
-					return true;
-				}
-			} else {
-				return false;
-			}
-		} else {
-			titorGridPosition = new GridPosition(titorGridPosition.getX() + 1, titorGridPosition.getY() + 1);
-			newGridPositionVerify();
-			return true;
-
-		}
-
-	}
+//	public void newGridPositionVerify() {
+//		towerList.clear();
+//		lock = false;
+//
+//		for (Tower i : towersInMap) {
+//
+//			if (i.canLockTarget(this)) {
+//
+//				towerList.add(i);
+//				lock = true;
+//			}
+//		}
+//		if (lock) {
+//			for (Tower i : towerList) {
+//				i.shootEnemy(this);
+//
+//			}
+//
+//		}
+//
+//	}
+//
+//	public boolean move() {
+//		if (titorGridPosition.getX() > -1 && titorGridPosition.getY() > -1) {
+//
+//			if (health > 0) {
+//				if (titorGridPosition.getX() + 1 < titorMap.length
+//						&& titorMap[titorGridPosition.getX() + 1][titorGridPosition.getY()] == '/') {
+//
+//					titorMap[titorGridPosition.getX() + 1][titorGridPosition.getY()] = '+';
+//					titorGridPosition = new GridPosition(titorGridPosition.getX() + 1, titorGridPosition.getY());
+//
+//				} else if (titorGridPosition.getX() > 0
+//						&& titorMap[titorGridPosition.getX() - 1][titorGridPosition.getY()] == '/') {
+//
+//					titorMap[titorGridPosition.getX() - 1][titorGridPosition.getY()] = '+';
+//					titorGridPosition = new GridPosition(titorGridPosition.getX() - 1, titorGridPosition.getY());
+//
+//				} else if (titorGridPosition.getY() + 1 < titorMap[0].length
+//						&& titorMap[titorGridPosition.getX()][titorGridPosition.getY() + 1] == '/') {
+//
+//					titorMap[titorGridPosition.getX()][titorGridPosition.getY() + 1] = '+';
+//					titorGridPosition = new GridPosition(titorGridPosition.getX(), titorGridPosition.getY() + 1);
+//
+//				} else if (titorGridPosition.getY() > 0
+//						&& titorMap[titorGridPosition.getX()][titorGridPosition.getY() - 1] == '/') {
+//
+//					titorMap[titorGridPosition.getX()][titorGridPosition.getY() - 1] = '+';
+//					titorGridPosition = new GridPosition(titorGridPosition.getX(), titorGridPosition.getY() - 1);
+//
+//				}
+//				newGridPositionVerify();
+//				if (titorGridPosition.getX() == endGridPosition.getX()
+//						&& titorGridPosition.getY() == endGridPosition.getY()) {
+//					return false;
+//				} else {
+//					return true;
+//				}
+//			} else {
+//				return false;
+//			}
+//		} else {
+//			titorGridPosition = new GridPosition(titorGridPosition.getX() + 1, titorGridPosition.getY() + 1);
+//			newGridPositionVerify();
+//			return true;
+//
+//		}
+//
+//	}
 
 	/**
 	 * Updates the state of the enemy by a given amount of time has passed
@@ -288,9 +286,9 @@ public class Enemy extends Observable {
 	 * @return the current position of the enemy on the map
 	 */
 	public Point2f getCurrentPosition() {
-		if (currentPosition == null) {
-			return new Point2f(titorGridPosition.getX(), titorGridPosition.getY());
-		}
+//		if (currentPosition == null) {
+//			return new Point2f(titorGridPosition.getX(), titorGridPosition.getY());
+//		}
 		return currentPosition;
 	}
 	
