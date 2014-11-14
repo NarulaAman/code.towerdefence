b9a28c43a2b9650e9b7f3be6e1588325d6d4c2d1
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-14 01:28:43 +0000

    Added comments to CannonTower file

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/CannonTower.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/CannonTower.java
@@ -4,22 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ca.concordia.soen6441.logic.Enemy;
+import ca.concordia.soen6441.logic.GameMap;
 import ca.concordia.soen6441.logic.TowerFactory;
 import ca.concordia.soen6441.logic.primitives.GridPosition;
 import ca.concordia.soen6441.logic.tower.shootingstrategy.ShootingStrategy;
-
+/**
+ * 
+ * This class has characteristics of Cannon {@link Tower}
+ *
+ */
 public class CannonTower extends Tower {
 	
 	private static final float SPLASH_RANGE = 1;
 	private static final float SPLASH_DAMAGE_RATIO = 0.3f;
 
 	private transient List<Enemy> enemies;
-	
+	/**
+	 * Calls the constructor of {@link Tower}
+	 * @param level The level of the game
+	 * @param gridPosition The position of the {@link Tower} on {@link GameMap}
+	 * @param shootingStrategy The object of {@link ShootingStrategy}
+	 * @param towerFactory The object of {@link TowerFactory}
+	 */
 	public CannonTower(int level, GridPosition gridPosition, ShootingStrategy shootingStrategy, TowerFactory towerFactory) {
 		super(level, gridPosition, shootingStrategy, towerFactory);
 	}
 
-	@Override
+/**
+ * Tower shoot the enemies 
+ * @param enemy {@link Enemy} in the range
+ */
 	protected void specializedShot(Enemy enemy) {
 		List<Enemy> enemiesToSplash = new ArrayList<>();
 		for (Enemy otherEnemy : enemies) {
@@ -37,12 +51,16 @@ public class CannonTower extends Tower {
 		}
 	}
 
-	@Override
+/**
+ * Visit this tower to with the {@link TowerVisitor}
+ */
 	public void visit(TowerVisitor visit) {
 		visit.visit(this);
 	}
 
-	
+/**
+* Shoot the {@link Enemy} after interval of time
+*/
 	public void maybeShoot(List<Enemy> shootEnemies) {
 		if (hasCooledDown()) {
 			enemies = shootEnemies;
