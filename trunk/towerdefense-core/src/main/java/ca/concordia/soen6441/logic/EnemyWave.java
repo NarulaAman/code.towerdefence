07cd7a6de1a278a4a02c692f0396ce1f8830c01a
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-14 00:46:39 +0000

    Added comments to EnemyWave file

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/EnemyWave.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logic/EnemyWave.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 import javax.vecmath.Point2f;
 
+/**
+ * 
+ * This class spawn {@link Enemy} into the {@link GamePlay}
+ */
 public class EnemyWave implements Serializable{
 	
 	private final GamePlay gamePlay;
@@ -17,7 +21,12 @@ public class EnemyWave implements Serializable{
 	private int enemiesToSpawn;
 	
 	private float lastSpawnDeltaTime;
-	
+	/**
+	 * Initialize the data members
+	 * @param gamePlay The object of {@link GamePlay}
+	 * @param spawnEverySeconds After number of seconds the {@link Enemy} should be spawn in {@link GamePlay}
+	 * @param enemiesToSpawn Number of {@link Enemy} to be spawn
+	 */
 	public EnemyWave(GamePlay gamePlay, float spawnEverySeconds, /*float startDelaySeconds, */ int enemiesToSpawn) {
 		super();
 		this.gamePlay = gamePlay;
@@ -28,13 +37,19 @@ public class EnemyWave implements Serializable{
 	}	
 	
 	
-	
+	/**
+	 * Spawn the {@link Enemy} into the {@link GamePlay}
+	 */
 	public void spawnEnemy() {
 		Enemy e = new Enemy(gameMap, 100, new Point2f(gameMap.getStartGridPosition().getX(), gameMap.getStartGridPosition().getY()));
 		gamePlay.addEnemy(e);
 		enemiesToSpawn = enemiesToSpawn - 1;
 	}
 	
+	/**
+	 * Spawn the {@link Enemy} into the {@link GamePlay} after interval of time 
+	 * @param seconds The number of seconds
+	 */
 	public void update(float seconds) {
 		lastSpawnDeltaTime = lastSpawnDeltaTime + seconds;
 		while (lastSpawnDeltaTime > spawnEverySeconds) {
@@ -42,7 +57,9 @@ public class EnemyWave implements Serializable{
 			spawnEnemy();
 		}
 	}
-	
+/*
+ * Check if all the enemies are spawned
+ */
 	public boolean isFinished() {
 		return enemiesToSpawn < 1;
 	}
