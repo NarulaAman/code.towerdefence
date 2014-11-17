a348a048ddb89076a87650a041f88aa2c0618e76
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-17 06:47:29 +0000

    Added comments to AbstractTempoeralEffectTower

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/AbstractTemporalEffectTower.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/tower/AbstractTemporalEffectTower.java
@@ -24,10 +24,10 @@ public abstract class AbstractTemporalEffectTower extends Tower implements Obser
 	
 	/**
 	 * Initialize the AbstractTemporalEffectTower  
-	 * @param level
-	 * @param gridPosition
-	 * @param shootingStrategy
-	 * @param towerFactory
+	 * @param level level of the tower to be created
+	 * @param gridPosition grid position of the tower
+	 * @param shootingStrategy shooting strategy to apply to the tower
+	 * @param towerFactory tower factory
 	 */
 	public AbstractTemporalEffectTower(int level, GridPosition gridPosition, ShootingStrategy shootingStrategy,
 			TowerFactory towerFactory) {
