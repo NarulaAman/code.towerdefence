9169262f4d61d050e7541332691a35ab71bc1d49
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-17 19:55:56 +0000

    Tag for Build2

---
+++ tags/Build2/towerdefense-core/src/test/java/ca/concordia/soen6441/logic/tower/FireTowerTest.java
@@ -0,0 +1,62 @@
+package ca.concordia.soen6441.logic.tower;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import ca.concordia.soen6441.logic.Enemy;
+import ca.concordia.soen6441.logic.TowerFactory;
+import ca.concordia.soen6441.logic.primitives.GridPosition;
+import ca.concordia.soen6441.logic.tower.FireTower;
+
+/**
+ * Tests the special effects of the {@link FireTower}
+ *
+ */
+public class FireTowerTest {
+
+	private final TowerFactory towerFactory = new TowerFactory();
+	
+	private FireTower tower;
+	private Enemy enemy;
+	
+	/**
+	 * Pre-conditions of the test
+	 */
+	@Before
+	public void setUp() {
+		tower = (FireTower) towerFactory.towerOnCoordinate(FireTower.class, new GridPosition(1, 1));
+		enemy = mock(Enemy.class);
+	}
+	
+	
+	/**
+	 * Test the burn damage of the {@link FireTower}
+	 */
+	@Test
+	public void testBurn() {
+		tower.shoot(enemy);
+		Mockito.verify(enemy).takeDamage(tower.getDamage());
+		Mockito.reset(enemy);
+		tower.update(tower.getBurnDurationSecs() + 0.1f);
+		Mockito.verify(enemy, times((int)(tower.getBurnDurationSecs() / tower.getBurnRateSecs()))).takeDamage(tower.getBurnDamage());
+	}
+	/**
+	 * Test the burn damage of the {@link FireTower} is not applied after the burning time
+	 */
+	@Test
+	public void testNotBurningAfterBurnDuration() {
+		tower.shoot(enemy);
+		Mockito.verify(enemy).takeDamage(tower.getDamage());
+		// first burn until the max burn duration
+		tower.update(tower.getBurnDurationSecs() + 0.1f);
+		// now the enemy shouldn't be burnt anymore
+		Mockito.reset(enemy);
+		tower.update(tower.getBurnDurationSecs() + 0.1f);
+		Mockito.verify(enemy, never()).takeDamage(tower.getBurnDamage());
+	}
+}
---
+++ tags/Build2/towerdefense-core/src/test/java/ca/concordia/soen6441/logic/tower/IceTowerTest.java
@@ -0,0 +1,53 @@
+package ca.concordia.soen6441.logic.tower;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyFloat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import ca.concordia.soen6441.logic.Enemy;
+import ca.concordia.soen6441.logic.TowerFactory;
+import ca.concordia.soen6441.logic.primitives.GridPosition;
+
+/**
+ * Tests the {@link IceTower} special effects
+ *
+ */
+public class IceTowerTest {
+
+	private final TowerFactory towerFactory = new TowerFactory();
+	
+	private IceTower tower;
+	private Enemy enemy;
+	
+	/**
+	 * Preconditions of all tests
+	 */
+	@Before
+	public void setUp() {
+		tower = (IceTower) towerFactory.towerOnCoordinate(IceTower.class, new GridPosition(1, 1));
+		enemy = mock(Enemy.class);
+	}
+	
+	
+	/**
+	 * Test the if the {@link IceTower} is slowing down the enemies
+	 */
+	@Test
+	public void testSlowingDownOfTheEnemies() {
+		ArgumentCaptor<Float> argument = ArgumentCaptor.forClass(Float.class);
+		Mockito.when(enemy.getSpeed()).thenReturn(10.f);
+		tower.shoot(enemy);
+		tower.update(tower.getSlownessDurationSecs() + 0.1f);
+		Mockito.verify(enemy, times(2)).setSpeed(argument.capture());
+		assertTrue(argument.getAllValues().get(0).floatValue() < 10.f);
+		assertTrue(argument.getAllValues().get(1).floatValue() > 9.9 && argument.getValue().floatValue() < 10.1);
+		Mockito.verify(enemy, times(2)).setSpeed(anyFloat()); // original speed was set
+	}
+
+}
