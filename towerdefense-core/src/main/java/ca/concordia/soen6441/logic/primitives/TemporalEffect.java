b1980b15f83354a803b235eefaeee8682a1d28c6
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-17 00:10:08 +0000

    Added comments to TemporalEffect file

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/primitives/TemporalEffect.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/primitives/TemporalEffect.java
@@ -1,21 +1,35 @@
 package ca.concordia.soen6441.logic.primitives;
 
+import ca.concordia.soen6441.logic.Enemy;
+
+/**
+ * This class will manage the temporary affect of {@link Tower} on {@link Enemy}
+ *
+ */
 public class TemporalEffect {
 	
 	boolean active = false;
 	float duration;
 	float timeLeft;
-	
+	/**
+	 * Initialize the data members
+	 */
 	public TemporalEffect(float duration) {
 		this.duration = duration;
 		timeLeft = duration;
 	}
 	
+	/**
+	 * Enable the affect on {@link Enemy}
+	 */
 	public void start() {
 		active = true;
 		startEffect();
 	}
-	
+	/**
+	 * Stop the affect if the specific time has passed
+	 * @param seconds The number of seconds passed
+	 */
 	public void update(float seconds) {
 		timeLeft = timeLeft - seconds;
 		if (isActive())
@@ -27,10 +41,17 @@ public class TemporalEffect {
 			}
 		}
 	}
+	/**
+	 * Return True if the affect is active
+	 * @return True if the affect is active
+	 */
 	public boolean isActive() {
 		return active;
 	}
-	
+	/**
+	 * Return True if the time is out   
+	 * @return Return true if the time is out
+	 */
 	public boolean isTimedOut() {
 		return timeLeft <= 0; 
 	}
