c0bf5a61d7c0e165fd7ef3630d18de4886c3fb22
alexandre.pretyman@gmail.com <alexandre.pretyman@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-17 06:47:44 +0000

    Added comments to TemporalEffect

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/primitives/TemporalEffect.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/primitives/TemporalEffect.java
@@ -14,6 +14,7 @@ public class TemporalEffect {
 	float timeLeft;
 	/**
 	 * Initialize the data members
+	 * @param duration duration of effect
 	 */
 	public TemporalEffect(float duration) {
 		this.duration = duration;
