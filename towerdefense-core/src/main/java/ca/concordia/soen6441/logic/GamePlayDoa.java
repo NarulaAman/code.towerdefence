b72b817f8bf2fc7ff830735da1cdb9d4a0156e82
mohamed.omar.t.a@gmail.com <mohamed.omar.t.a@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-21 23:46:43 +0000

    Initiated save load functions in GamePlayDoa class to save the whole game

---
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/GamePlayDoa.java
@@ -0,0 +1,18 @@
+package ca.concordia.soen6441.logic;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public interface GamePlayDoa {
+	
+	public void save(GamePlay gamePlay) throws IOException;
+	
+	
+	public GameMap load(String name) throws IOException,
+	ClassNotFoundException;
+	
+	
+	public List<String> listAllNames() throws IOException;
+
+}
