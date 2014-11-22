06c773d53551dbaceab5217b200b1308738c3246
mohamed.omar.t.a@gmail.com <mohamed.omar.t.a@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-22 00:47:15 +0000

    Created GameMapDao Test class to test save load and listAllNames methods in GameMapDao class which overrided in GamePlayJavaSerialaizationDao class

---
+++ towerdefense-core/src/test/java/ca/concordia/soen6441/logic/GamePlayDaoTest.java
@@ -0,0 +1,39 @@
+package ca.concordia.soen6441.logic;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+//import ca.concordia.soen6441.io.GamePlayJavaSerializationDao;
+//import ca.concordia.soen6441.logic.primitives.GridPosition;
+
+/**
+ * Test cases for the {@link GameMapDao}
+ *
+ */
+@FixMethodOrder(MethodSorters.JVM)
+
+public class GamePlayDaoTest {
+
+	@Test
+	public void testSave() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoad() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListAllNames() {
+		//fail("Not yet implemented");
+	}
+
+}
