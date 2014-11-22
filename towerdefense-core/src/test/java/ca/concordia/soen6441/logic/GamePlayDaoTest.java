ffa7f6d8d1775acb7659a001591685f33c97a486
mohamed.omar.t.a@gmail.com <mohamed.omar.t.a@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-22 01:26:44 +0000

    Added get and set name to the GamePlay so we can have more than one saved GamePlay files.

--- towerdefense-core/src/test/java/ca/concordia/soen6441/logic/GamePlayDaoTest.java
+++ towerdefense-core/src/test/java/ca/concordia/soen6441/logic/GamePlayDaoTest.java
@@ -1,28 +1,59 @@
 package ca.concordia.soen6441.logic;
 
-import java.io.File;
-import java.io.IOException;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
+import static org.junit.Assert.*;
+
 import org.junit.BeforeClass;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
 
-//import ca.concordia.soen6441.io.GamePlayJavaSerializationDao;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ca.concordia.soen6441.io.GameMapJavaSerializationDao;
+import ca.concordia.soen6441.io.GamePlayJavaSerialaizationDao;
 import ca.concordia.soen6441.logic.primitives.GridPosition;
 
-/**
- * Test cases for the {@link GameMapDao}
- *
- */
-@FixMethodOrder(MethodSorters.JVM)
 public class GamePlayDaoTest {
 	
-	private static final String MAP_DATA_FILENAME = "testMapName";
+
+	private static final String GAMEPLAY_DATA_FILENAME = "testMapName";
 	
-	//private GamePlayJavaSerializationDao gameMapDao = new GameMapJavaSerializationDao();
+	private GamePlayJavaSerialaizationDao gamePlayDao = new GamePlayJavaSerialaizationDao();
+	//private GameMapJavaSerializationDao gameMapDao = new GameMapJavaSerializationDao();
 	private static GameMap gameMap = new GameMap(32, 32);
 	
+	private static GamePlay gamePlay = new GamePlay(gameMap, 500);
+
+	/**
+	 * Creates a {@link GameMap} to aid the tests
+	 * @throws Exception on error
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		gamePlay.setName(GAMEPLAY_DATA_FILENAME);
+		gameMap.setTile(new GridPosition(1, 2), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(5, 6), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(2, 27), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(5, 8), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(3, 13), Tile.ENEMY_PATH);
+		gameMap.setTile(new GridPosition(15, 26), Tile.ENEMY_PATH);
+		
+	}
+
+	@Test
+	public void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListAllNames() {
+		fail("Not yet implemented");
+	}
+
 }
