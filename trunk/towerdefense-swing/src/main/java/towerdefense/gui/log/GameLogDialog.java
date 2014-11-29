9190fb5c2794e7131e850300d92c8599621043eb
amand.narula@gmail.com <amand.narula@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-29 21:45:58 +0000

    Added MapLogAction and MapLogDialog for map logs

--- trunk/towerdefense-swing/src/main/java/towerdefense/gui/log/GameLogDialog.java
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/log/GameLogDialog.java
@@ -2,12 +2,16 @@ package towerdefense.gui.log;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.inject.Inject;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JScrollPane;
@@ -21,21 +25,26 @@ import ca.concordia.soen6441.logger.LogMessage;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-public class GameLogDialog extends JDialog implements Observer {
+public class GameLogDialog extends JDialog implements Observer,ActionListener {
 
 	private final JTable table = new JTable();
 
 	private final JScrollPane scrollPane = new JScrollPane();
 	
-	private final JComboBox<String> comboBox = new JComboBox<String>();
 	private final LogMessageTableModel logMessageTableModel;
 	
+	private final DefaultComboBoxModel<LogFilter> logFilterModel = new DefaultComboBoxModel<LogFilter>();
+
+	private final JComboBox<LogFilter> comboBox = new JComboBox<LogFilter>(logFilterModel);
 	private final LogManager logManager;
 	
 	private Object displayed = null;
 	
 	@Inject
 	public GameLogDialog(LogMessageTableModel logMessageTableModel, LogManager logManager) {
+		
+		setMaximumSize(new Dimension(600, 450));
+		setMinimumSize(new Dimension(600, 450));
 		this.logMessageTableModel = logMessageTableModel;
 		this.logManager = logManager;
        
@@ -45,8 +54,10 @@ public class GameLogDialog extends JDialog implements Observer {
 		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
 		table.setFillsViewportHeight(true);
 		logMessageTableModel.setLogMessages(logManager.getLogsFor(new LogFilter("sdf")));
-
+        comboBox.addActionListener(this);
+		update(null, null);
 		logMessageTableModel.fireTableDataChanged();
+		add(comboBox,BorderLayout.NORTH);
 		add(table, BorderLayout.CENTER);
 		pack();
 		setVisible(true);
@@ -56,23 +67,26 @@ public class GameLogDialog extends JDialog implements Observer {
 	public void displayLogs(Object object) {
 		if (object != displayed) {
 			displayed = object;
-			showLogsFor(object);
+			//showLogsFor(object);
 		}
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		if (arg == null) {
-			logMessageTableModel.setLogMessages(new ArrayList<LogMessage>());
-		} else if (arg == displayed) {
-			showLogsFor(arg);
+		LogFilter selected = (LogFilter) logFilterModel.getSelectedItem();
+		logFilterModel.removeAllElements();
+		for (LogFilter filter : logManager.getLogFilters()) {
+			logFilterModel.addElement(filter);
 		}
+		//logFilterModel.setSelectedItem(selected);
 	}
 
 
-	private void showLogsFor(Object arg) {
-		logMessageTableModel.setLogMessages(logManager.getLogsFor(new LogFilter("sdf")));
-	}
+
+//	private void showLogsFor(Object arg) {
+//		logMessageTableModel.setLogMessages(logManager.getLogsFor(displayed));
+//	}
+
 	
 //	/**
 	//	 * Creates the GUI for testing purposes
@@ -113,6 +127,15 @@ public class GameLogDialog extends JDialog implements Observer {
 					}
 				}
 			});
+		}
+
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JComboBox<LogFilter> logFilters = (JComboBox<LogFilter>)e.getSource();
+	        LogFilter logFilter = (LogFilter)logFilters.getSelectedItem();
+	        List<LogMessage> logMessages = logManager.getLogsFor(logFilter);
+	        logMessageTableModel.setLogMessages(logMessages);
 		}
 
 	
---
+++ trunk/towerdefense-swing/src/main/java/towerdefense/gui/log/MapLogDialog.java
@@ -0,0 +1,82 @@
+package towerdefense.gui.log;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Observable;
+
+import javax.inject.Inject;
+import javax.swing.JDialog;
+import javax.swing.JTable;
+
+import towerdefense.gui.guice.GuiModule;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import ca.concordia.soen6441.logger.LogFilter;
+import ca.concordia.soen6441.logger.LogManager;
+import ca.concordia.soen6441.logic.GameMap;
+
+public class MapLogDialog extends JDialog {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final JTable table = new JTable();
+	
+	private final LogMessageTableModel logMessageTableModel;
+	private final LogManager logManager;
+	private GameMap selectedMap;
+	
+	@Inject
+	public MapLogDialog(LogMessageTableModel logMessageTableModel, LogManager logManager) {
+		
+		setMaximumSize(new Dimension(600, 450));
+		
+		setMinimumSize(new Dimension(600, 450));
+		this.logMessageTableModel = logMessageTableModel;
+		this.logManager = logManager;
+       
+		table.setModel(logMessageTableModel);
+
+		// scrollPane.add(table);
+		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
+		table.setFillsViewportHeight(true);
+		logMessageTableModel.setLogMessages(logManager.getLogsFor(new LogFilter(selectedMap.toString())));
+		logMessageTableModel.fireTableDataChanged();
+		add(table, BorderLayout.CENTER);
+		pack();
+		setVisible(true);
+	}
+	
+	public void mapSelected(GameMap selectedMap) {
+		this.selectedMap = selectedMap;
+	}
+	
+	private static void createAndShowGUI() throws ClassNotFoundException, IOException {
+		
+		Injector injector = Guice.createInjector(new GuiModule());
+		MapLogDialog mapLogPanel = injector.getInstance(MapLogDialog.class);  
+    }
+	
+	public static void main(String args[]) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					createAndShowGUI();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+}
