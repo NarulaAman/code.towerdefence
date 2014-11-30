d92fa40084485001913ea382aead5d4ddccfb430
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-30 01:18:37 +0000

    Added comments to log.java

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logger/Log.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logger/Log.java
@@ -5,8 +5,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * The Interface Log.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Log {
+	
+	/**
+	 * Value.
+	 *
+	 * @return the string
+	 */
 	String value() default "";
 }
