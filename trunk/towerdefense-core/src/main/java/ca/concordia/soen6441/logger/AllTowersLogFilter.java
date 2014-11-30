acb45f864a92d1fca03697fc9d0a94fa308a813c
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-30 01:17:59 +0000

    Added comments to AllTowersLogFilter

--- trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logger/AllTowersLogFilter.java
+++ trunk/towerdefense-core/src/main/java/ca/concordia/soen6441/logger/AllTowersLogFilter.java
@@ -2,14 +2,23 @@ package ca.concordia.soen6441.logger;
 
 import ca.concordia.soen6441.logic.tower.Tower;
 
+
+/**
+ * The Class AllTowersLogFilter.
+ */
 public class AllTowersLogFilter extends LogFilter {
 
+	/**
+	 * Instantiates a new all towers log filter.
+	 */
 	public AllTowersLogFilter() {
 		super("All Towers");
 
 	}
 	
-	
+/**
+ * This method will filter 
+ */
 	boolean filter(LogMessage logMessage) {
 		return logMessage.getSource() instanceof Tower;
 	}
