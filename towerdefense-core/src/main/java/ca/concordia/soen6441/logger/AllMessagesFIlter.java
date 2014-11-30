32a242f88e62ed19b2686e6be3e918ce195589d1
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-30 01:17:09 +0000

    Added Comments to AllMessagesFilter

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logger/AllMessagesFIlter.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logger/AllMessagesFIlter.java
@@ -1,16 +1,24 @@
 package ca.concordia.soen6441.logger;
 
+
+/**
+ * The Class AllMessagesFIlter.
+ */
 public class AllMessagesFIlter extends LogFilter {
 
-	/**
-	 * 
-	 */
+	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 556940165605471976L;
 
+	/**
+	 * Instantiates a new all messages filter.
+	 */
 	public AllMessagesFIlter() {
 		super("All Gameplay Log");
 	}
 	
+	/**
+	 * This method will filter 
+	 */
 	boolean filter(LogMessage logMessage) {
 		return true;
 	}
