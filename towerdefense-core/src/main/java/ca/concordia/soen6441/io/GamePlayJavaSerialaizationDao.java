8bab4feea36531fff65569e6370bca47b71dd36f
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-30 01:14:33 +0000

    Added Comments to GamePlayJavaSerialaizationDao.java

--- towerdefense-core/src/main/java/ca/concordia/soen6441/io/GamePlayJavaSerialaizationDao.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/io/GamePlayJavaSerialaizationDao.java
@@ -12,12 +12,20 @@ import java.util.List;
 import ca.concordia.soen6441.logic.GamePlay;
 import ca.concordia.soen6441.logic.GamePlayDao;
 
+/**
+ * The Class GamePlayJavaSerialaizationDao.
+ */
 public class GamePlayJavaSerialaizationDao implements GamePlayDao {
 	
+	/** The Constant GAMEPLAY_FILENAME_EXTENSION. */
 	public static final String GAMEPLAY_FILENAME_EXTENSION = ".game";
 	
+	/** The Constant GAMEFILENAME_STRING_FORMAT. */
 	public static final String GAMEFILENAME_STRING_FORMAT = "." + File.separator + "%s" + GAMEPLAY_FILENAME_EXTENSION;
 	
+	/**
+	 * This method will save the GamePlay
+	 */
 	@Override
 	public void save(GamePlay gamePlay) throws IOException {
 		File file = new File(String.format(GAMEFILENAME_STRING_FORMAT, gamePlay.getName())); 
@@ -29,6 +37,9 @@ public class GamePlayJavaSerialaizationDao implements GamePlayDao {
 	}
 	
 	
+/**
+ * This method will load the GamePlay
+ */
 	@Override
 	public GamePlay load(String gamePlayName) throws IOException, ClassNotFoundException {
 		File file = new File(String.format(GAMEFILENAME_STRING_FORMAT, gamePlayName));
@@ -40,6 +51,9 @@ public class GamePlayJavaSerialaizationDao implements GamePlayDao {
 		return gamePlay;
 	}
 	
+/**
+ * This method will list the name of all GamePlay
+ */
 	@Override
 	public List<String> listAllNames() throws IOException {
 		File directory = new File(".");
