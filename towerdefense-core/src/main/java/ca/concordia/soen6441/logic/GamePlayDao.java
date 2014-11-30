ff528d034531d12e740987d97855afefb169f657
jitinsharda87@gmail.com <jitinsharda87@gmail.com@54ace2af-5297-f09d-e1d5-f75d18bc50a2>
2014-11-30 01:30:37 +0000

    Added Comments to files

--- towerdefense-core/src/main/java/ca/concordia/soen6441/logic/GamePlayDao.java
+++ towerdefense-core/src/main/java/ca/concordia/soen6441/logic/GamePlayDao.java
@@ -4,15 +4,39 @@ import java.io.IOException;
 import java.util.List;
 
 
+
+/**
+ * The Interface GamePlayDao.
+ */
 public interface GamePlayDao {
 	
+	/**
+	 * Save.
+	 *
+	 * @param gamePlay the game play
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public void save(GamePlay gamePlay) throws IOException;
 	
 	
+	/**
+	 * Load.
+	 *
+	 * @param name the name
+	 * @return the game play
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws ClassNotFoundException the class not found exception
+	 */
 	public GamePlay load(String name) throws IOException,
 	ClassNotFoundException;
 	
 	
+	/**
+	 * List all names.
+	 *
+	 * @return the list
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
 	public List<String> listAllNames() throws IOException;
 
 }
